fc2e4ce7292db1f47e07353277ef069bc7ccbe11
Kayler C <kmcookhorn@gmail.com>
2018-08-23 13:48:56 -0400

    final edits.  need to push to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,24 @@
 posts = [
-  {fname: 'Jon', lname: 'Doe', email: 'e@example.com'},
-  {fname: 'Jane', lname: 'Doe', email: 'e@example.com'}
+  {username: 'j_bosslady',url: 'lorem pixum', content: 'lorem'},
+  {username: 'Jess_dabest', url: 'lorem pixum', content: 'lorem'}
+  {username: 'StephStayBlesssed', url: 'lorem pixum', content: 'lorem'}
 ]
 
 posts.each do |p|
   Post.create(p)
+end
+
+
+
+
+
+require 'faker'
+
+25.times do
+  Post.create (
+    username: Faker::Name.username,
+    img: Faker::Img.
+    content: Faker:
+  )
+
 end
